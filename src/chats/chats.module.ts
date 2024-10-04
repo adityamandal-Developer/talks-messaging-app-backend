71cf05920c966d61770610fe1994dc4c7ce25ae6
adityamandal-Developer <mandaladitya901@gmail.com>
2024-10-04 13:42:57 +0530

    multiple subscriptions added

---
+++ src/chats/chats.module.ts
@@ -0,0 +1,18 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { ChatsService } from './chats.service';
+import { ChatsResolver } from './chats.resolver';
+import { DatabaseModule } from '../common/database/database.module';
+import { Chat } from './entities/chat.entity';
+import { ChatsRepository } from './chats.repository';
+import { MessagesModule } from './messages/messages.module';
+import { ChatSchema } from './entities/chat.document';
+
+@Module({
+  imports: [
+    DatabaseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    forwardRef(() => MessagesModule),
+  ],
+  providers: [ChatsResolver, ChatsService, ChatsRepository],
+  exports: [ChatsRepository],
+})
+export class ChatsModule {}
